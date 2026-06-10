0e141f0bb92856dc3813a04ab22e4fb744af4691
Louis Sanyenni <lsanyenni@gmail.com>
2026-06-10 19:49:22 +0000

    jwtservices

--- src/main/java/com/codewithlouis/codefest_project/services/JwtServices.java
+++ src/main/java/com/codewithlouis/codefest_project/services/JwtServices.java
@@ -1,4 +1,15 @@
 package com.codewithlouis.codefest_project.services;
 //
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtServices {
+    @Value("${security.jwt.secert-key}")
+    private String secertKey;
+
+
+
+
 }
