7811d4eff14756cba6ee7f8c1c2d7f1639fe3a69
Louis Sanyenni <lsanyenni@gmail.com>
2026-06-20 21:07:42 +0000

    Pitche entity created

--- src/main/java/com/codewithlouis/codefest_project/model/User.java
+++ src/main/java/com/codewithlouis/codefest_project/model/User.java
@@ -45,7 +45,7 @@ public class User implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
-
+//
     public String getPassword() {
         return password;
     }
