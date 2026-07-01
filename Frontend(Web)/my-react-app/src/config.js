d0a384ca446c0ec2b40f3f1c4471b8bf7b0bd782
Louis Sanyenni <lsanyenni@gmail.com>
2026-07-01 21:42:29 +0000

    changes

--- Frontend(Web)/my-react-app/src/config.js
+++ Frontend(Web)/my-react-app/src/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://codefest-backend.onrender.com";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 export default BASE_URL;
